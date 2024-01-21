a6cccd3ab5ff560333c10a33c16bcb5a2ff9a611
shinyay <shinya.com@gmail.com>
2024-01-21 23:18:20 +0900

    Add a function to find templat file path

---
+++ works/Day008/sample/view/console.py
@@ -0,0 +1,12 @@
+import os
+import string
+import termcolor
+
+def get_template_dir_path():
+    template_dir_path = None
+
+    if not template_dir_path:
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        template_dir_path = os.path.join(base_dir, 'templates')
+
+    return template_dir_path
