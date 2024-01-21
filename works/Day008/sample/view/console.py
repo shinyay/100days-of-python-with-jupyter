4bdf4e612bc4ce04189b2510dc0183510844cb29
shinyay <shinya.com@gmail.com>
2024-01-21 23:30:50 +0900

    Add a function to get template

--- works/Day008/sample/view/console.py
+++ works/Day008/sample/view/console.py
@@ -17,3 +17,16 @@ def find_template(temp_file):
     if not os.path.exists(temp_file_path):
         raise NoTemplateError('Could Not Find: {}'.format(temp_file))
     return temp_file_path
+
+class NoTemplateError(Exception):
+    """No Template Error"""
+
+def get_template(temp_file_path, color=None):
+    template = find_template(temp_file_path)
+    with open(template, 'r', encoding='utf-8') as template_file:
+        contents = template_file.read()
+        contents = contents.rstrip(os.linesep)
+        contents = '{splitter}{sep}{contents}{sep}{splitter}{sep}'.format(
+            contents=contents, splitter="=" * 60, sep=os.linesep)
+        contents = termcolor.colored(contents, color)
+        return string.Template(contents)
