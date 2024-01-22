87168640d0fad1951ed5178d34d91a1f7fa86eba
shinyay <shinya.com@gmail.com>
2024-01-22 18:53:32 +0900

    Add CsvModel clsss

--- works/Day008/sample/model/ranking.py
+++ works/Day008/sample/model/ranking.py
@@ -7,3 +7,8 @@ RANKING_COLUMN_NAME = 'NAME'
 RANKING_COLUMN_COUNT = 'COUNT'
 RANKING_CSV_FILE_PATH = 'ranking.csv'
 
+class CsvModel:
+    def __init__(self, csv_file):
+        self.csv_file = csv_file
+        if not os.path.exists(csv_file):
+            pathlib.Path(csv_file).touch()
