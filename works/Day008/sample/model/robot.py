45c6abd215ca07eb93b8c83183e23fd93a083dec
shinyay <shinya.com@gmail.com>
2024-01-22 19:05:50 +0900

    Add recommend_restaurant for Restauraurant Class

--- works/Day008/sample/model/robot.py
+++ works/Day008/sample/model/robot.py
@@ -30,3 +30,28 @@ class RestaurantRobot(Robot):
                 self.hello()
             return func(self)
         return wrapper
+
+    @_hello_decorator
+    def recommend_restaurant(self):
+        new_recommend_restaurant = self.ranking_model.get_most_popular()
+        if not new_recommend_restaurant:
+            return None
+
+        will_recommend_restaurants = [new_recommend_restaurant]
+        while True:
+            template = console.get_template('greeting.txt', self.speak_color)
+            is_yes = input(template.substitute({
+                'robot_name': self.name,
+                'user_name': self.user_name,
+                'restaurant': new_recommend_restaurant
+            }))
+
+            if is_yes.lower() == 'y' or is_yes.lower() == 'yes':
+                break
+
+            if is_yes.lower() == 'n' or is_yes.lower() == 'no':
+                new_recommend_restaurant = self.ranking_model.get_most_popular(
+                    not_list=will_recommend_restaurants)
+                if not new_recommend_restaurant:
+                    break
+                will_recommend_restaurants.append(new_recommend_restaurant)
