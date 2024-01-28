14455123c11aceb5b255701e2b33476f20680301
shinyay <shinya.com@gmail.com>
2024-01-28 20:32:57 +0900

    Add a flask code with render_template

--- works/Day011/hello_flask_template.py
+++ works/Day011/hello_flask_template.py
@@ -3,8 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    user_name = 'John Doe'
+    return 'Hello, Flask'
+
+
+@app.route('/hello')
 def hello():
     user_name = 'John Doe'
+    return render_template('index.html', username=user_name)
+
+@app.route('/hello')
+@app.route('/hello/<user_name>')
+def hello2(user_name=None):
     return render_template('index.html', username=user_name)
 
 if __name__ == '__main__':
