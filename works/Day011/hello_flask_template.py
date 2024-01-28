66ade774dfd07caffdba95e62b0b9d3c26d3057c
shinyay <shinya.com@gmail.com>
2024-01-28 20:41:02 +0900

    Add post method

--- works/Day011/hello_flask_template.py
+++ works/Day011/hello_flask_template.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -17,6 +18,10 @@ def hello():
 @app.route('/hello/<user_name>')
 def hello2(user_name=None):
     return render_template('index.html', username=user_name)
+
+@app.route('/post', methods=['POST', 'PUT', 'DELETE'])
+def show_post():
+    return str(request.values['username'])
 
 if __name__ == '__main__':
     app.run()
