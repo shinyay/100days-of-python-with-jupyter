aeaaf44a580f15107e2d2b92cbfed9fcbbb4b36b
shinyay <shinya.com@gmail.com>
2024-01-28 20:13:52 +0900

    Add a flask code with render_template

---
+++ works/Day011/hello_flask_template.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    user_name = 'John Doe'
+    return render_template('index.html', username=user_name)
+
+if __name__ == '__main__':
+    app.run()
