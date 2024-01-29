4d3f56a94d14536a639ced76dc7ea3a4db073dcd
shinyay <shinya.com@gmail.com>
2024-01-29 19:28:23 +0900

    Add teardown_appcontext decorator

--- works/Day011/hello_database.py
+++ works/Day011/hello_database.py
@@ -1,8 +1,14 @@
 import sqlite3
+from flask import Flask
 from flask import g
+
+app = Flask(__name__)
 
 def get_db():
     db = getattr(g,'_database', None)
     if db is None:
         db = g._database = sqlite3.connect('test_sqlite.db')
     return db
+
+@app.teardown_appcontext
+def close_connectopn(exception)
