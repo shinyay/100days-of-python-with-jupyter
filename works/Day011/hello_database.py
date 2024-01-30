461cfb12dd1b0e58ae646f6754ffb40ae0993622
shinyay <shinya.com@gmail.com>
2024-01-30 17:28:56 +0900

    Modify syntax

--- works/Day011/hello_database.py
+++ works/Day011/hello_database.py
@@ -6,7 +6,7 @@ from flask import request
 app = Flask(__name__)
 
 def get_db():
-    db = getattr(g,'_database', None)
+    db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect('test_sqlite.db')
     return db
